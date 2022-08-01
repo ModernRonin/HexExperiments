f7576bf69d81aaf782c0c11ce37a69d2cfd78f50
Mark Hajek <markhajek@outlook.com>
2022-08-01 17:31:33 +0100

    drawing multiple rings of hexes

--- Viewer/HexView.cs
+++ Viewer/HexView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,7 +20,11 @@ public class HexView : Control
         ctx.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1d),
             new Rect(0, 0, RenderSize.Width, RenderSize.Height));
 
-        drawHex(new Point(RenderSize.Width / 2, RenderSize.Height / 2), 10);
+        foreach (var coordinate in Coordinates)
+        {
+            var point = coordinate.ToPoint(10) + new Vector(RenderSize.Width / 2, RenderSize.Height / 2);
+            drawHex(point, 10);
+        }
 
         void drawHex(Point center, double scale)
         {
@@ -38,5 +43,42 @@ public class HexView : Control
                 return new Point(center.X + scale * Math.Cos(angle), center.Y + scale * Math.Sin(angle));
             }
         }
+    }
+
+    static IEnumerable<HexCoordinate> Coordinates
+    {
+        get
+        {
+            const int rings = 3;
+            for (var q = -rings; q < rings + 1; ++q)
+            {
+                for (var r = -rings; r < rings + 1; ++r)
+                {
+                    var point = new HexCoordinate(q, r);
+                    if (Math.Abs(point.S) <= rings) yield return point;
+                }
+            }
+        }
+    }
+}
+
+public readonly struct HexCoordinate
+{
+    static readonly double _rootOf3 = Math.Sqrt(3);
+    public int Q { get; }
+    public int R { get; }
+    public int S => -Q - R;
+
+    public HexCoordinate(int q, int r)
+    {
+        Q = q;
+        R = r;
+    }
+
+    public Point ToPoint(double scale)
+    {
+        var x = scale * (_rootOf3 * Q + _rootOf3 / 2d * R);
+        var y = scale * (3d       / 2d * R);
+        return new Point(x, y);
     }
 }
