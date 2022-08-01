ecd08664d82ea72a987178e7002b26ca1494de0d
Mark Hajek <markhajek@outlook.com>
2022-08-01 16:55:59 +0100

    Add project files.

---
+++ Viewer/HexView.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Viewer;
+
+public class HexView : Control
+{
+    static HexView()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(HexView),
+            new FrameworkPropertyMetadata(typeof(HexView)));
+    }
+
+    protected override void OnRender(DrawingContext ctx)
+    {
+        ctx.DrawRectangle(Brushes.Black, new Pen(Brushes.Black, 1d),
+            new Rect(0, 0, RenderSize.Width, RenderSize.Height));
+    }
+}
