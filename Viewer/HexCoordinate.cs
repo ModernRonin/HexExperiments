3c3ee23a12ea68e18462abdeee809724b1ff624a
Mark Hajek <markhajek@outlook.com>
2022-08-02 11:05:17 +0100

    split HexCoordinate into its own file

---
+++ Viewer/HexCoordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+
+namespace Viewer;
+
+public readonly struct HexCoordinate
+{
+    static readonly double _rootOf3 = Math.Sqrt(3);
+    public int Q { get; }
+    public int R { get; }
+    public int S => -Q - R;
+
+    public HexCoordinate(int q, int r)
+    {
+        Q = q;
+        R = r;
+    }
+
+    public Point ToPoint(double scale)
+    {
+        var x = scale * (_rootOf3 * Q + _rootOf3 / 2d * R);
+        var y = scale * (3d       / 2d * R);
+        return new Point(x, y);
+    }
+}
