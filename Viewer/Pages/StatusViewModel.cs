f31ad2f2470f349365378c310b6964467a809681
Mark Hajek <markhajek@outlook.com>
2022-08-02 14:28:59 +0100

    forgotten change

--- Viewer/Pages/StatusViewModel.cs
+++ Viewer/Pages/StatusViewModel.cs
@@ -1,6 +1,8 @@
-﻿namespace Viewer.Pages;
+﻿using PropertyChanged.SourceGenerator;
 
-public class StatusViewModel
+namespace Viewer.Pages;
+
+public partial class StatusViewModel
 {
     [Notify] HexCoordinate? _underMouse;
     public string UnderMouseText => UnderMouse?.ToString() ?? "n/a";
