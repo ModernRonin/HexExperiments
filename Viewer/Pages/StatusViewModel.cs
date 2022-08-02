661e829ab0d447d55ad86f136a85164577d5e270
Mark Hajek <markhajek@outlook.com>
2022-08-02 13:29:24 +0100

    binding StatusBarVM.UnderMouseText

--- Viewer/Pages/StatusViewModel.cs
+++ Viewer/Pages/StatusViewModel.cs
@@ -9,7 +9,11 @@ public class StatusViewModel : PropertyChangedBase
     public HexCoordinate? UnderMouse
     {
         get => _underMouse;
-        set => SetAndNotify(ref _underMouse, value);
+        set
+        {
+            SetAndNotify(ref _underMouse, value);
+            NotifyOfPropertyChange(nameof(UnderMouseText));
+        }
     }
 
     public string UnderMouseText => _underMouse?.ToString() ?? "n/a";
