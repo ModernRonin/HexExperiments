07092b729dd9caf04009181fb1083914a026bb29
Mark Hajek <markhajek@outlook.com>
2022-08-05 19:51:07 +0100

    increased cell count to provoke render performance issues

--- Viewer/Pages/HexConfigurationViewModel.cs
+++ Viewer/Pages/HexConfigurationViewModel.cs
@@ -5,7 +5,7 @@ namespace Viewer.Pages;
 
 public partial class HexConfigurationViewModel : Screen
 {
-    [Notify] int _ringCount = 5;
+    [Notify] int _ringCount = 50;
     public void Cancel() => RequestClose();
     public void Confirm() => RequestClose(true);
 }
