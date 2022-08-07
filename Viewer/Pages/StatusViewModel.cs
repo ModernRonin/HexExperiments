564d821918a91a9927801d779fa41c7475a7a14d
Mark Hajek <markhajek@outlook.com>
2022-08-07 11:41:24 +0100

    added FPS counter display

--- Viewer/Pages/StatusViewModel.cs
+++ Viewer/Pages/StatusViewModel.cs
@@ -5,7 +5,9 @@ namespace Viewer.Pages;
 
 public partial class StatusViewModel
 {
+    [Notify] int _framerate;
     [Notify] HexCoordinate? _underMouse;
     [Notify] float _zoom;
+    public string FramerateText => $"{Framerate} FPS";
     public string UnderMouseText => UnderMouse?.ToString() ?? "n/a";
 }
